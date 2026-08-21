6794b8fa7a03b3854986c5ac280b8c6941856850
mhmdfirza <m.firza.aqila.r@gmail.com>
2026-08-21 22:50:20 +0700

    add

---
+++ index.php
@@ -0,0 +1,13 @@
+<?php
+// index.php
+$greeting = "Hello, World!";
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>My Native PHP Project</title>
+</head>
+<body>
+    <h1><?php echo $greeting; ?></h1>
+</body>
+</html>
